e0ab1f4ccc72c26b2453ca002e271b0509e4dc6b
jessehliu <jessehliu@msn.com>
2015-02-21 01:33:51 -0800

    Add documentation to the website

--- DevelopingDataProduct/server.r
+++ DevelopingDataProduct/server.r
@@ -2,20 +2,20 @@ library(shiny)
 
 doPrediction <- function(idx)
 {
-	data_train <- faithful[-idx, ];
-	data_testing <- faithful[idx, ];
-	eruption.lm = lm(eruptions ~ waiting, data=data_train)
+    data_train <- faithful[-idx, ];
+    data_testing <- faithful[idx, ];
+    eruption.lm = lm(eruptions ~ waiting, data=data_train)
 
-	predict_value = predict(eruption.lm, data_testing)
+    predict_value = predict(eruption.lm, data_testing)
 }
 
 shinyServer(
-	function(input, output)
-	{
-		output$actValue <- renderText({ faithful[input$idx, 1] })
-		output$predictValue <- renderText({
-			input$submitButton
-			isolate( doPrediction(input$idx) )
-		})
-	}
+    function(input, output)
+    {
+        output$actValue <- renderText({ faithful[input$idx, 1] })
+        output$predictValue <- renderText({
+            input$submitButton
+            isolate( doPrediction(input$idx) )
+        })
+    }
 )
--- DevelopingDataProduct/ui.r
+++ DevelopingDataProduct/ui.r
@@ -1,15 +1,22 @@
 library(shiny)
-shinyUI(pageWithSidebar(
-	headerPanel("Developing Data Product Course Project"),
-	sidebarPanel(
-		h3('The index of sample to predict'),
-		numericInput('idx', 'The index of data to be used for prediction', 100, min = 1, max = 272, step = 1),
-		actionButton("submitButton", "Click button to start prediction")
-	),
-	mainPanel(
-		h3('Actual value'),
-		verbatimTextOutput("actValue"),
-		h3('Predicted value'),
-		verbatimTextOutput("predictValue")
-	)
+shinyUI(navbarPage("Project",
+    tabPanel("Main page",
+        sidebarLayout(
+            sidebarPanel(
+                h3('Choose the sample to be used for prediction'),
+                numericInput('idx', 'The index of data to be choosen', 100, min = 1, max = 272, step = 1),
+                actionButton("submitButton", "Click button to start prediction")
+            ),
+            mainPanel(
+                h3('The actual eruption time'),
+                verbatimTextOutput("actValue"),
+                h3('The predicted eruption'),
+                verbatimTextOutput("predictValue")
+            )
+        )
+    ),
+    tabPanel("Documentation",
+        h3('Documentation'),
+        p('This webpage is to predict the duration of eruption based on waiting time between eruptions of Old Failthful geyser. The faithful dataset is usd. There are 272 observation in total. User can use the index of data to choose the sample to be used as testing data and the remaining 271 data will be used for training. The webpage will return the actual duration of eruption and predicted duration of eruption')
+    )
 ))
